f1a50dc47bc72d48b5211b8fe7cd010f330bee9a
padraicmannion <pjmannion86@gmail.com>
2016-07-07 11:33:09 +0100

    made changes
    need to fix line 66

--- filechecker.py
+++ filechecker.py
@@ -1,24 +1,43 @@
 # Take in a file named changes_python.txt and assign it to the variable fname.  Next are some list creations using []
-# for commits and authors and I created a dictionary using {} for commits_for_each_author.
+# for commits and author and I created a dictionary using {} for commits_for_each_author.
 # and initializer values for current_commit.  The variable reading_file opens the file from fname in read mode
 # and uses the method readlines to read the file.  Next index which I will use to move onto the next commits
-# in the file is initialized at 0.
-
+# in the file is initialized at 0.  The variable sep looks for lines that have 72 - followed by a new line.
 fname = 'changes_python.txt'
-
-commits = []
-current_commit = None
-commits_for_each_author = {}
-authors = []
 reading_file = open(fname, 'r')
 data = reading_file.readlines()
+commits = []
+commits_for_each_author = {}
+authors = []
 index = 0
+sep = 72*'-' +'\n'
 
-# The variable sep looks for lines that have 72 - followed by a new line.  A while loop is used to
-# go through the file.  First each commit is parsed and put into a list of commits.  split('|') separates
-# the lines by the pipe character, details[1].strip removes the spaces at the end of the word, author not in
-# author is used to check if the authors name is already in the list or is it a new author.  Next author[author] = []
-# gives the user an empty commit array.  Dictionaries are created for all the variables.
+
+# Creates a class called commiting with sets the initialization of the variables to $None.  When using the function
+# it uses __init__ for making the function private.  init and foo are common words used when creating these
+# functions.
+class Commiting:
+    def __init__(self, revision=None, author=None, date=None, comment_line_count=None, changes=None, comment=None):
+        self.revision = revision
+        self.author = author
+        self.date = date
+        self.comment_line_count = comment_line_count
+        self.changes = changes
+        self.comment = comment
+
+
+    # The get_commit_comment is used for when the details variables of the commit are returned and printed
+    # by using str the variables are parsed as strings.
+    def get_commit_comment(self):
+        return 'Merge the -r' + str(self.revision - 1) + ':' + str(self.revision) + ' by ' \
+        + self.author + ' with the comment ' + ','.join(self.comment) \
+        + ' and the changes in the new version are ' + ','.join(self.changes)
+
+
+# A while loop is used to go through the file.  First each commit is parsed and put into a list of commits.
+# split('|') separates the lines by the pipe character, details[1].strip removes the spaces at the end of the word,
+# author not in author is used to check if the authors name is already in the list or is it a new author.
+# Next author[author] = [] gives the user an empty commit array.  Dictionaries are created for all the variables.
 # It sets up the revision variable as with author the whitespace is removed.
 #
 # The commits are added to the list and index is used to find the next index by looking for the 72 hyphons.
@@ -30,29 +49,27 @@ index = 0
 # The comment takes the comment from the current comment.  The append function takes in the current_commit
 # the file is on in the loop and appends it to the list named commits.
 
-sep = 72*'-' +'\n'
+
 while True:
     try:
+        current_commit = Commiting()
         details = data[index + 1].split('|')
         author = details[1].strip()
         if author not in authors:
             authors.append(author)
             commits_for_each_author[author] = []
 
-        revision = details[0].strip()
-        commits_for_each_author[author].append(revision)
-        commits.append(details)
-        current_commit.date = []
-        current_commit.date.append(details[2].strip())
-        current_commit.comment_line_count = []
+        current_commit.revision = int(details[0].strip().strip('r'))
+        current_commit.author = details[1].strip()
+        current_commit.date = details[2].strip()
         current_commit.comment_line_count = int(details[3].strip().split(' ')[0])
-        current_commit.date = []
-        current_commit.changes = data[index + 2:data.index('', index + 1)]
+        current_commit.changes = data[index+2:data.index('',index+1)]
         index = data.index(sep, index + 1)
         current_commit.comment = data[index - current_commit.comment_line_count:index]
         commits.append(current_commit)
     except IndexError:
         break
+
 
 # Reverse is used on the commits list to sort the order, the list is printed and the no. of commits.  Next it prints
 # the number of elements in the authors list counting the number of authors and converting it to a string to print
--- filetest.py
+++ filetest.py
@@ -4,10 +4,10 @@
 # versioning =
 #################################################################################################################
 
-# The unittest library is used to perform the test cases.
+# The unittest library is used to perform the test cases and the class Commiting from filechecker is imported.
 
 import unittest
-#from filechecker import
+from filechecker import Commiting
 
 
 class TestFile(unittest.TestCase):
@@ -17,30 +17,18 @@ class TestFile(unittest.TestCase):
         # the setUp function uses the standard practice of naming the variable self to call itself.
         # def setUp(self).
 
-    def test_file_author_commits(self):
-        # Function to test the authors commits starts by looking for an author that isn't in the list in this case it
-        # will return 0 because the author will have 0 commits.  After I tested for specific authors from
-        # which I knew what the results of no of commits are e.g. dred0001 commited 2, murari.krishnan commited 1,
-        # robert.hanaway commited$ 152 and viacheslav.vdovenko commited 191.
-        self.filechecker.move(15)
-        self.assertEqual(15, self.filechecker.getMileage())
-        self.filechecker.move(12)
-        self.assertEqual(27, self.filechecker.getMileage())
-        self.filechecker.move(13)
-        self.assertEqual(40, self.filechecker.getMileage())
-        self.filechecker.move(3.5)
-        self.assertEqual(43.5, self.filechecker.getMileage())
+    def test_file_commits_details(self):
+        # Function that calls itself and is used to test the details variable
+        self.assertEqual( ,self.filechecker.get)
 
     def test_file_author_count(self):
         # Function for testing the number of authors in the list which is 10.
-        self.filechecker.setMake('Ferrari')
-        self.assertEqual('Ferrari', self.filechecker.getMake())
+        self.assertEqual(10, self.filechecker.get())
 
 
     def test_file_number_of_commits(self):
         # The value for len(commits) is 422.  Function for testing the number of commits in the file.
-        self.filechecker.setEngineSize(1.8)
-        self.assertEqual(1.8, self.filechecker.getEngineSize())
+        self.assertEqual(422, self.filechecker.get())
 
 # function for revison, author, date, comment, line_count and changes needed
 
