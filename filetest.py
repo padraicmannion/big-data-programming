75602bc99ebf566d274b88f405858fe66f3be7c5
padraicmannion <pjmannion86@gmail.com>
2016-07-08 10:20:43 +0100

    Possible test cases
    Researched possible test cases for the program

--- filetest.py
+++ filetest.py
@@ -32,7 +32,19 @@ class TestFile(unittest.TestCase):
 
 # function for revison, author, date, comment, line_count and changes needed
 
+# assertDictContainsSubset(expected, actual, msg=None)
+# Tests whether the key/value pairs in dictionary actual are a superset of those in expected. If not,
+# an error message listing the missing keys and mismatched values is generated.
 
+# assertItemsEqual(actual, expected, msg=None)
+# Test that sequence expected contains the same elements as actual, regardless of their order. When
+# they don’t, an error message listing the differences between the sequences will be generated.
+# Duplicate elements are not ignored when comparing actual and expected. It verifies if each element
+# has the same count in both sequences. It is the equivalent of assertEqual(sorted(expected),
+#sorted(actual)) but it works with sequences of unhashable objects as well.
+#http://nullege.com/codes/search/unittest.TestCase.assertDictContainsSubset
+#http://nullege.com/codes/search?cq=unittest.TestCase.assertCountEqual
+#http://nullege.com/codes/search?cq=unittest.TestCase.addTypeEqualityFunc
 
 # Runs all the test cases.
 if __name__ == '__main__':
